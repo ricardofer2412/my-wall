78e9f95214ad80fded2f6211517999b9351eecca
Richard Casanova <ricardofer2412@hotmail.com>
2020-06-30 00:07:18 -0400

    Final Version

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,98 +1,94 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import firebase from './firebase'
-import Container from '@material-ui/core/Container';
-
-
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import firebase from "./firebase";
+import Container from "@material-ui/core/Container";
+import { withRouter } from "react-router-dom";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" >
-        SilverLogic
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
+      {"Copyright © "}
+      <Link color="inherit">SilverLogic</Link> {new Date().getFullYear()}
+      {"."}
     </Typography>
   );
 }
 
 const classes = {
   paper: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
-  avatar: {
-  },
+  avatar: {},
   form: {
-    width: '100%',
+    width: "100%",
   },
   submit: {
-    backgroundColor: '#33B8FF'
+    backgroundColor: "#33B8FF",
   },
   mainContainer: {
-    marginTop: 100
+    marginTop: 100,
   },
   link: {
     textDecoration: "underline",
     color: "lightblue",
-    cursor: "pointer"
-  }
-
-}
+    cursor: "pointer",
+  },
+};
 
 class Register extends React.Component {
   constructor(props) {
-    super(props)
-
+    super(props);
 
     this.state = {
-      email: '',
-      password: '',
-      name: '',
-      errorMessage: ''
-    }
+      email: "",
+      password: "",
+      name: "",
+      errorMessage: "",
+    };
   }
 
-  signUp = (e) => {
-    e.preventDefault()
-    const { history } = this.props;
+  signUp = async (e) => {
+    e.preventDefault();
 
-    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(res => {
-        const { uid } = res.user;
-        console.log(uid)
-        firebase.firestore().collection('accounts').doc(uid).set({
-          userId: uid,
-          name: this.state.name,
-          email: this.state.email
-        })
-        history.push("/");
+    try {
+      const res = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password);
 
-      }).catch((error) => {
-        console.log(error.code);
-        console.log(error.message);
-        this.setState({
-          errorMessage: error.message
-        })
+      const { uid } = res.user;
+      console.log(uid);
+
+      await firebase.firestore().collection("accounts").doc(uid).set({
+        userId: uid,
+        name: this.state.name,
+        email: this.state.email,
       });
 
+      console.log("HERE");
 
-  }
+      window.location.href = "/";
+    } catch (error) {
+      console.log(error.code);
+      console.log(error.message);
+      this.setState({
+        errorMessage: error.message,
+      });
+    }
+  };
 
-  onChange = e => {
+  onChange = (e) => {
     const state = this.state;
     state[e.target.name] = e.target.value;
     this.setState(state);
@@ -107,10 +103,9 @@ class Register extends React.Component {
       <Container style={classes.mainContainer} component="main" maxWidth="xs">
         <CssBaseline />
         <div className={classes.paper}>
-
           <Typography component="h1" variant="h5">
             Sign Up
-      </Typography>
+          </Typography>
           <form className={classes.form} noValidate>
             <TextField
               variant="outlined"
@@ -120,7 +115,6 @@ class Register extends React.Component {
               id="name"
               label="Name"
               name="name"
-
               autoFocus
               onChange={this.onChange}
               value={this.state.name}
@@ -152,7 +146,9 @@ class Register extends React.Component {
               value={this.state.password}
             />
             {this.state.errorMessage && (
-              <Typography style={{ color: 'red' }}>{this.state.errorMessage}</Typography>
+              <Typography style={{ color: "red" }}>
+                {this.state.errorMessage}
+              </Typography>
             )}
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
@@ -167,20 +163,16 @@ class Register extends React.Component {
               onClick={this.signUp}
             >
               Sign Up
-        </Button>
+            </Button>
             <Typography>
               Already have an account?{" "}
               <span onClick={this.goToLogin} style={classes.link}>
                 Login instead
-                </span>
+              </span>
             </Typography>
             <Grid container>
-              <Grid item xs>
-
-              </Grid>
-              <Grid item>
-
-              </Grid>
+              <Grid item xs></Grid>
+              <Grid item></Grid>
             </Grid>
           </form>
         </div>
@@ -188,10 +180,8 @@ class Register extends React.Component {
           <Copyright />
         </Box>
       </Container>
-    )
+    );
   }
+}
 
-};
-
-
-export default Register;
+export default withRouter(Register);
--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,229 +1,227 @@
-import React from 'react'
-import Fab from '@material-ui/core/Fab';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Button from '@material-ui/core/Button';
-import firebase from './firebase'
-import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
-import CardContent from '@material-ui/core/CardContent';
-import Container from '@material-ui/core/Container';
-import Add from '@material-ui/icons/Add';
-import Delete from '@material-ui/icons/Delete';
+import React from "react";
+import Fab from "@material-ui/core/Fab";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Button from "@material-ui/core/Button";
+import firebase from "./firebase";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import CardContent from "@material-ui/core/CardContent";
+import Container from "@material-ui/core/Container";
+import Add from "@material-ui/icons/Add";
+import Delete from "@material-ui/icons/Delete";
 import { useHistory, withRouter } from "react-router-dom";
-import Tooltip from '@material-ui/core/Tooltip';
+import Tooltip from "@material-ui/core/Tooltip";
 
 const uuid = require("uuid");
 const classes = {
-
   postList: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   },
   card: {
     marginButton: 20,
     marginTop: 20,
-    width: 500
-
+    width: 500,
   },
   dialogText: {
     width: 500,
-    hight: 300
+    hight: 300,
   },
   buttonEnable: {
     margin: 0,
-    top: 'auto',
+    top: "auto",
     right: 20,
     bottom: 20,
-    left: 'auto',
-    position: 'fixed',
-    color: 'white',
-    backgroundColor: '#33B8FF'
+    left: "auto",
+    position: "fixed",
+    color: "white",
+    backgroundColor: "#33B8FF",
   },
   buttonDisable: {
     margin: 0,
-    top: 'auto',
+    top: "auto",
     right: 20,
     bottom: 20,
-    left: 'auto',
-    position: 'fixed',
-    color: '#33B8FF',
-    backgroundColor: '#ECF2F5'
+    left: "auto",
+    position: "fixed",
+    color: "#33B8FF",
+    backgroundColor: "#ECF2F5",
   },
   deleteIcon: {
-    color: '#14486'
+    color: "#14486",
   },
   userName: {
-    color: '#144864'
+    color: "#144864",
   },
   postText: {
-    fontSize: 25
+    fontSize: 25,
   },
   cardButtom: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-
-  }
-
-}
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+};
 
 class Wall extends React.Component {
   constructor(props) {
-    super(props)
-    this.postRef = firebase.firestore().collection("posts")
+    super(props);
+    this.postRef = firebase.firestore().collection("posts");
 
     this.state = {
-      postId: '',
-      content: '',
-      userId: '',
-      key: '',
+      postId: "",
+      content: "",
+      userId: "",
+      key: "",
       postsList: [],
-      user: '',
+      user: "",
       open: false,
-      currentUser: '',
-      postedBy: '',
-      postedById: ''
-    }
+      currentUser: "",
+      postedBy: "",
+      postedById: "",
+    };
   }
-
 
   // Get Post and Current Logged in user!
   componentDidMount() {
-    this.getPosts()
-    this.currentUser()
+    this.getPosts();
+    this.currentUser();
   }
 
   // Function to get Users from Database
   currentUser = () => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        const userId = user.uid
-        firebase.firestore().collection("accounts")
+        const userId = user.uid;
+        firebase
+          .firestore()
+          .collection("accounts")
           .doc(userId)
           .get()
           .then((doc) => {
             if (doc.exists) {
-              console.log(doc.data().name)
-              console.log(doc.data().userId)
               this.setState({
                 postedBy: doc.data().name,
-                currentUser: doc.data().userId
-              })
+                currentUser: doc.data().userId,
+              });
             } else {
-              console.log('Document does not exist')
+              console.log("Document does not exist");
             }
-          })
+          });
       }
-    })
-  }
-
+    });
+  };
 
   // Function to get Posts from Database
   getPosts() {
-    const postsList = []
-    this.postRef.get().then(querySnapshot => {
-      querySnapshot.forEach(doc => {
-        const { content, postedBy } = doc.data()
-        postsList.push(
-          {
-            key: doc.id,
-            content,
-            postedBy
-          }
-        )
-      })
+    const postsList = [];
+    this.postRef.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const { content, postedBy, postedById, date } = doc.data();
+        postsList.push({
+          key: doc.id,
+          content,
+          postedBy,
+          postedById,
+          date: date,
+        });
+      });
       this.setState({
-        postsList
-      })
-    })
-    return postsList
-
+        postsList,
+      });
+    });
+    return postsList;
   }
-  onChange = e => {
+  onChange = (e) => {
     const state = this.state;
     state[e.target.name] = e.target.value;
     this.setState(state);
   };
 
-
   //Function to create posts
   addPost = (e) => {
-    e.preventDefault()
-    const newPostsList = [...this.state.postsList,
-    { content: this.state.content, key: this.state.key, postedBy: this.state.postedBy, postedById: this.state.postedById }]
-    const postId = uuid()
-    this.postRef.doc(postId).set({
+    e.preventDefault();
+    const date = Date.now();
+    const uid = this.props.user.uid;
+    const postId = uuid();
+
+    const newPost = {
       content: this.state.content,
       postId: postId,
+      key: postId,
       postedBy: this.state.postedBy,
-      date: Date.now(),
-      postedById: this.state.currentUser
-    })
-      .then(res => {
+      date,
+      postedById: uid,
+    };
+
+    const newPostsList = [...this.state.postsList, newPost];
+    this.postRef
+      .doc(postId)
+      .set(newPost)
+      .then((res) => {
         this.setState({
           postsList: newPostsList,
-          content: '',
-          postId: '',
-          postedBy: '',
-          postedById: ''
-        })
-      })
-    this.handleClickClose()
-    this.getPosts()
-  }
+          content: "",
+          postId: "",
+          postedById: "",
+        });
+      });
+    this.handleClickClose();
+    this.getPosts();
+  };
 
-
-  //delete post by postId 
+  //delete post by postId
   deletePost = (id) => {
-    this.postRef.doc(id).delete().then(() => {
-      this.getPosts()
-    })
-      .catch((error) => {
-        console.log('Error removing document: ', error)
+    this.postRef
+      .doc(id)
+      .delete()
+      .then(() => {
+        this.getPosts();
       })
+      .catch((error) => {
+        console.log("Error removing document: ", error);
+      });
     // Delete from state
-  }
-
+  };
 
   handleClickOpen = () => {
     this.setState({
-      open: true
-    })
-  }
+      open: true,
+    });
+  };
   handleClickClose = () => {
     this.setState({
-      open: false, content: '',
-    })
-  }
+      open: false,
+      content: "",
+    });
+  };
   disabledPost = () => {
-    alert("Need to Login to post")
-  }
-
-
+    alert("Need to Login to post");
+  };
 
   render() {
-    const { user } = this.props
+    const { user } = this.props;
     return (
       <div>
         <Dialog
           open={this.state.open}
-          onClose={this.handleClickClose} aria-labelledby="form-dialog-title">
+          onClose={this.handleClickClose}
+          aria-labelledby="form-dialog-title"
+        >
           <DialogTitle id="form-dialog-title">Add New Post</DialogTitle>
           <DialogContent>
-            <DialogContentText>
-            </DialogContentText>
+            <DialogContentText></DialogContentText>
             <TextField
               style={classes.dialogText}
               autoFocus
-              InputProps={{ name: 'content' }}
+              InputProps={{ name: "content" }}
               onChange={this.onChange}
               value={this.state.content}
               multiline
@@ -237,58 +235,77 @@ class Wall extends React.Component {
           <DialogActions>
             <Button onClick={this.handleClickClose} color="primary">
               Cancel
-          </Button>
-            <Button id="create_post_dialog_button" onClick={this.addPost} color="primary" type='submit'>
+            </Button>
+            <Button
+              id="create_post_dialog_button"
+              onClick={this.addPost}
+              color="primary"
+              type="submit"
+            >
               Add
-          </Button>
+            </Button>
           </DialogActions>
         </Dialog>
         <Container style={classes.button}>
           <Tooltip title="Add Post" aria-label="add">
             {user ? (
-              <Fab id="create_post_button" onClick={this.handleClickOpen} style={classes.buttonEnable} aria-label=" add">
+              <Fab
+                id="create_post_button"
+                onClick={this.handleClickOpen}
+                style={classes.buttonEnable}
+                aria-label=" add"
+              >
                 <Add />
               </Fab>
             ) : (
-                <Fab onClick={this.disabledPost} style={classes.buttonDisable} aria-label=" add">
+                <Fab
+                  onClick={this.disabledPost}
+                  style={classes.buttonDisable}
+                  aria-label=" add"
+                >
                   <Add />
                 </Fab>
-              )
-            }
+              )}
           </Tooltip>
         </Container>
 
         {/* This will render all post  */}
         <Container style={classes.postList}>
-          {this.state.postsList.map(post =>
-            <Card style={classes.card}>
-              <CardContent>
-
-                <Typography style={classes.postText} color="textSecondary" gutterBottom>
-                  {post.content}
-                </Typography>
-
-                <Container style={classes.cardButtom}>
-
-                  {/* This will disable anothe user to delete, other user's post */}
-
-                  <Typography style={classes.userName} gutterBottom>
-                    @{post.postedBy}
+          {this.state.postsList
+            .sort((a, b) => b.date - a.date)
+            .map((post) => (
+              <Card style={classes.card}>
+                <CardContent>
+                  <Typography
+                    style={classes.postText}
+                    color="textSecondary"
+                    gutterBottom
+                  >
+                    {post.content}
                   </Typography>
-                  {user && this.state.postedBy === post.postedBy && (
-                    <Tooltip title="Delete Post" aria-label="add">
-                      < Delete onClick={() => this.deletePost(post.key)} variant='contained' style={{ color: '#144864' }} />
-                    </Tooltip>
-                  )}
-                </Container>
 
-              </CardContent>
-            </Card>
-          )}
+                  <Container style={classes.cardButtom}>
+                    {/* This will disable anothe user to delete, other user's post */}
+
+                    <Typography style={classes.userName} gutterBottom>
+                      @{post.postedBy}
+                    </Typography>
+                    {user && user.uid === post.postedById && (
+                      <Tooltip title="Delete Post" aria-label="add">
+                        <Delete
+                          onClick={() => this.deletePost(post.key)}
+                          variant="contained"
+                          style={{ color: "#144864", cursor: "pointer" }}
+                        />
+                      </Tooltip>
+                    )}
+                  </Container>
+                </CardContent>
+              </Card>
+            ))}
         </Container>
-      </div >
-    )
+      </div>
+    );
   }
-
 }
 export default Wall;
